14b2ff05162eac0ec5e10387d93bd4f6fd9480be
Alemol <alemolamg@user.github.com>
2019-11-21 12:04:14 +0100

    Ver 5.2.3: creando el primo, falla al comprobar que es primo

--- Pra05/THashCliente.cpp
+++ Pra05/THashCliente.cpp
@@ -11,9 +11,14 @@
  * Created on 19 de noviembre de 2019, 11:43
  */
 
+#include <queue>
+
 #include "THashCliente.h"
 
-THashCliente::THashCliente() {
+THashCliente::THashCliente(unsigned long tamTabla):
+    tamFisico(tamTabla),tamLogico(0),totalColisiones(0),
+    maxColisiones(0),tabla(tamTabla,Entrada()){
+    //primo
 }
 
 THashCliente::THashCliente(const THashCliente& orig) {
@@ -21,4 +26,25 @@ THashCliente::THashCliente(const THashCliente& orig) {
 
 THashCliente::~THashCliente() {
 }
+
+unsigned long THashCliente::calcPrimo(unsigned long tam) {
+    unsigned long elPrimo; elPrimo=tam+1;
+    bool encontrado=false;
+    do{
+        if(esprimo(elPrimo)==true){
+            if(tam/elPrimo>=0.60)
+                encontrado=true;
+            else
+                ++elPrimo;
+        }              
+    }while(!encontrado);
+    return elPrimo;
+}
+
+bool esprimo(unsigned long n) {
+        for (unsigned long i = 2; i <= n/2; ++i)
+            if (n % i == 0)
+                return false;
+        return true;
+    }
 
